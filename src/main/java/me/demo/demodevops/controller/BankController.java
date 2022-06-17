ede045b0c1bf503acfa7da0bac1b14b9b49ddc6c
Taeyoon Nam <tom@icbnet.co.kr>
2022-06-17 23:01:54 +0900

    feat : healthCheck API 개발

--- src/main/java/me/demo/demodevops/controller/BankController.java
+++ src/main/java/me/demo/demodevops/controller/BankController.java
@@ -1,7 +1,6 @@
 package me.demo.demodevops.controller;
 
-import me.demo.demodevops.entity.Bank;
-import me.demo.demodevops.service.BankService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,37 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
  * @date : 2022/06/10
  * @description :
  */
+@Log4j2
 @RestController
 public class BankController {
 
-  @Autowired
-  private BankService bankService;
+//  @Autowired
+//  private BankService bankService;
 
   @GetMapping("/healthCheck")
   public String healthCheck() {
+    log.info("*** healthCheck Test");
     return "Ok";
   }
 
-  @PostMapping("/accounts")
-  public ResponseEntity makeAccount(@RequestBody Bank bank) {
-    return ResponseEntity.ok(bankService.makeAccount(bank));
+  @GetMapping("/testApi")
+  public String testApi() {
+    log.info("*** testApi Test");
+    return "Test Ok";
   }
 
-  @DeleteMapping("/accounts")
-  public ResponseEntity deleteAccount(@RequestParam(name = "accountNum", required = true) long accountNum) {
-    bankService.deleteAccount(accountNum);
-    return ResponseEntity.status(HttpStatus.OK).build();
-  }
-
-  @PatchMapping("/accounts")
-  public ResponseEntity updateAccountInfo(@RequestBody Bank bank) {
-    bankService.updateAccountInfo(bank);
-    return ResponseEntity.status(HttpStatus.OK).build();
-  }
-
-  @GetMapping("/accounts")
-  public ResponseEntity getAccountInfo(@RequestParam(name = "accountNum", required = true) long accountNum) {
-    return ResponseEntity.ok(bankService.getAccountInfo(accountNum));
-  }
+//  @PostMapping("/accounts")
+//  public ResponseEntity makeAccount(@RequestBody Bank bank) {
+//    return ResponseEntity.ok(bankService.makeAccount(bank));
+//  }
+//
+//  @DeleteMapping("/accounts")
+//  public ResponseEntity deleteAccount(@RequestParam(name = "accountNum", required = true) long accountNum) {
+//    bankService.deleteAccount(accountNum);
+//    return ResponseEntity.status(HttpStatus.OK).build();
+//  }
+//
+//  @PatchMapping("/accounts")
+//  public ResponseEntity updateAccountInfo(@RequestBody Bank bank) {
+//    bankService.updateAccountInfo(bank);
+//    return ResponseEntity.status(HttpStatus.OK).build();
+//  }
+//
+//  @GetMapping("/accounts")
+//  public ResponseEntity getAccountInfo(@RequestParam(name = "accountNum", required = true) long accountNum) {
+//    return ResponseEntity.ok(bankService.getAccountInfo(accountNum));
+//  }
 
 }
--- src/test/java/me/demo/demodevops/controller/BankControllerTest.java
+++ src/test/java/me/demo/demodevops/controller/BankControllerTest.java
@@ -1,45 +1,45 @@
-package me.demo.demodevops.controller;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-import me.demo.demodevops.service.BankService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-/**
- * @author : naming
- * @packageName : me.demo.demodevops.controller
- * @date : 2022/06/11
- * @description :
- */
-@WebMvcTest(BankController.class)
-public class BankControllerTest {
-
-  @MockBean
-  private BankService bankService;
-
-  @Autowired
-  private MockMvc mockMvc;
-
-  @Test
-  void test() throws Exception {
-    MvcResult result = mockMvc.perform(post("/accounts")
-        .contentType(MediaType.APPLICATION_JSON_VALUE)
-        .content("{\n"
-            + "    \"accountNum\": 110220330440,\n"
-            + "    \"accountNm\": \"junitTest\",\n"
-            + "    \"amount\": 99999\n"
-            + "}"))
-        .andReturn();
-
-    assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
-  }
-
-}
+//package me.demo.demodevops.controller;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//
+//import me.demo.demodevops.service.BankService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//
+///**
+// * @author : naming
+// * @packageName : me.demo.demodevops.controller
+// * @date : 2022/06/11
+// * @description :
+// */
+//@WebMvcTest(BankController.class)
+//public class BankControllerTest {
+//
+//  @MockBean
+//  private BankService bankService;
+//
+//  @Autowired
+//  private MockMvc mockMvc;
+//
+//  @Test
+//  void test() throws Exception {
+//    MvcResult result = mockMvc.perform(post("/accounts")
+//        .contentType(MediaType.APPLICATION_JSON_VALUE)
+//        .content("{\n"
+//            + "    \"accountNum\": 110220330440,\n"
+//            + "    \"accountNm\": \"junitTest\",\n"
+//            + "    \"amount\": 99999\n"
+//            + "}"))
+//        .andReturn();
+//
+//    assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+//  }
+//
+//}
